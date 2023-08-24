fed8728b22d3783058fc20f234d1d3990a531576
GhaniAlZyad <ghanialzyad@gmail.com>
2023-08-24 12:58:54 +0700

    update "main.py"

--- App/main.py
+++ App/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from .database import db
 
-app = FastAPI()
+def init_app():
+    db.init()
+    app = FastAPI(
+        tittle="website gafystore",
+        descriptions="toko credit game online",
+        version="1"
+    )
+    @app.on_event("startup")
+    async def startup():
+        await db.create_all()
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
 
-@app.get("/ping")
-async def pong():
-    return {"ping": "pong"}
+    from .src.controllers import user_controller
+
+    @app.get('/health-check')
+    async def pong():
+        return {'ping':'pong'}
+        
+    app.include_router(
+        user_controller.api,
+        prefix="/api/v1"
+        )
+    return app
+
+app = init_app()
